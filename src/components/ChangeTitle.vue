e65d8062a3f8270340b51d64ed796d6cf776a451
KienKhang <trankienkhang2210@gmail.com>
2022-10-13 14:50:14 +0700

    test: success test unit and component

---
+++ src/components/ChangeTitle.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="text-center w-96">
+    <h1 class="font-medium">{{ valueHeading }}</h1>
+    <div class="flex w-full justify-between">
+      <input
+        type="text"
+        class="input p-3 border-green-100 outline-none focus:border-blue-500"
+        placeholder="Please input"
+        v-model="valueInput"
+      />
+      <button
+        @click="valueHeading = valueInput"
+        class="px-2 py-4 rounded-lg bg-green-500"
+      >
+        Submit
+      </button>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+  import { Ref, ref } from 'vue'
+
+  const valueInput: Ref<string> = ref('')
+  const valueHeading: Ref<string> = ref('')
+</script>
+
+<style scoped></style>
