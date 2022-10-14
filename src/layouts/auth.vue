88376f3d291b9efb0fabd8f1e47cd981718b989c
KienKhang <trankienkhang2210@gmail.com>
2022-10-14 17:21:21 +0700

    feat: create auth and home layout

---
+++ src/layouts/auth.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="bg-red-500"><router-view /></div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
---
+++ src/layouts/home.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="bg-blue-500">
+    <router-view />
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
