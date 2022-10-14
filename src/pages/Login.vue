5548c251812dc01d3afe1b1781f6bcff9b160e30
NguyenTu1601 <95576770+nguyenvantu15@users.noreply.github.com>
2022-10-14 12:08:23 +0700

    feat: validate form login

--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -12,19 +12,43 @@
       </div>
       <form class="p-10 w-1/2 block absolute top-0 right-0">
         <p class="font-semibold text-lg mb-7">Login</p>
-        <input
-          class="border-b-[1px] mb-8 w-full h-10 outline-none border-b-slate-500"
-          placeholder="Username"
-          id="username"
-          v-model="username"
-        />
-        <input
-          type="password"
-          class="border-b-[1px] mb-8 w-full h-10 outline-none border-b-slate-500"
-          placeholder="Password"
-          id="password"
-          v-model="password"
-        />
+        <div class="relative">
+          <input
+            class="border-b-[1px] w-full h-10 outline-none border-b-slate-500 mb-2"
+            :class="{
+              ' border-solid border-red-700 border-[1px] border-b-red-700':
+                v$.username.$error,
+            }"
+            placeholder="Username"
+            id="username"
+            v-model="formData.username"
+          />
+          <!-- <span v-if="v$.username.$error == false" class="block"> </span> -->
+          <span
+            class="block text-red-700 font-semibold"
+            v-for="error in v$.username.$errors"
+            >{{ error.$message }}</span
+          >
+        </div>
+        <div>
+          <input
+            type="password"
+            class="border-b-[1px] w-full h-10 outline-none border-b-slate-500 mb-2"
+            :class="{
+              ' border-solid border-red-700 border-[1px] border-b-red-700':
+                v$.password.$error,
+            }"
+            placeholder="Password"
+            id="password"
+            v-model="formData.password"
+          />
+
+          <span
+            class="mb-5 block text-red-700 font-semibold"
+            v-for="error in v$.password.$errors"
+            >{{ error.$message }}</span
+          >
+        </div>
 
         <div class="flex mb-8">
           <input class="w-5 h-5" type="checkbox" v-model="rememberMe" />
@@ -32,7 +56,8 @@
         </div>
         <button
           class="px-7 text-white drop-shadow-md py-2 bg-green-600 rounded"
-          @click="handleLogin($event)"
+          @click="submitForm($event)"
+          :disabled="!v$"
         >
           LOGIN
         </button>
@@ -42,14 +67,31 @@
 </template>
 
 <script setup lang="ts">
-  import { ref } from 'vue'
-  let rememberMe = ref(false)
-  let username = ref('')
-  let password = ref('')
+  import { reactive, ref } from 'vue'
+  import { useVuelidate } from '@vuelidate/core'
+  import { required, minLength } from '@vuelidate/validators'
 
-  function handleLogin(event) {
-    event.preventDefault()
-    alert(username.value + ' ' + password.value + ' ' + rememberMe.value)
+  const formData = reactive({
+    username: '',
+    password: '',
+  })
+
+  const rules = {
+    username: { required },
+    password: { required, minLength: minLength(8) },
   }
+
+  const v$ = useVuelidate(rules, formData)
+
+  const submitForm = async (event) => {
+    event.preventDefault()
+    const isFormCorrect = await v$.value.$validate()
+    if (isFormCorrect) {
+      alert('submit')
+    }
+    console.log({ isFormCorrect })
+  }
+
+  let rememberMe = ref(false)
 </script>
 <style scoped></style>
